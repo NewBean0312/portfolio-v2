35ffc4564f5ee5e0fc3049624311d32841635cae
NewBean0312 <newbean0312@gmail.com>
2024-04-02 12:11:27 +0900

    Tailwind CSS 커스텀 컬러 적용

---
+++ src/styles/theme.js
@@ -0,0 +1,6 @@
+const customColor = {
+  mainColor: "#416d83",
+  backgroundColor: "#f3f4e9",
+  textColor: "#122a70",
+  focusColor: "#023df7",
+};
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+      colors: {
+        transparent: 'transparent',
+        mainColor: "#416d83",
+        backgroundColor: "#f3f4e9",
+        textColor: "#122a70",
+        focusColor: "#023df7",
+    }
+  },
+  plugins: [],
+};
